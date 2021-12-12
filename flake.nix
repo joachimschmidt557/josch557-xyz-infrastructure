6ba310f82fb033e05994a38eab78442cba4cfd2c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-12-12 12:55:45 +0100

    Move to nix flakes

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+
+  outputs = { self, nixpkgs }: {
+
+    nixopsConfigurations.default = {
+
+      inherit nixpkgs;
+
+      network = {
+        description = "production environment";
+        enableRollback = true;
+        storage.legacy.databasefile = "~/.nixops/deployments.nixops";
+      };
+
+      goodsprings =
+        { config, pkgs, ... }:
+        {
+          deployment.targetHost = "goodsprings.josch557.de";
+          imports = [ ./hosts/goodsprings.nix ];
+        };
+
+    };
+
+  };
+
+}
