026b2fe044814f945b6b051b508bb045fa676a3f
joachimschmidt557 <joachim.schmidt557@outlook.com>
2023-12-26 14:44:18 +0800

    Update nixos-mailserver to 23.11 and update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  inputs.simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-23.05";
+  inputs.simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-23.11";
 
   outputs = { self, nixpkgs, simple-nixos-mailserver }: {
 
