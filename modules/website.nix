92f257bf3f580869e243c7a5f77034dfcc13a874
joachimschmidt557 <joachim.schmidt557@outlook.com>
2024-10-26 19:26:52 +0200

    website: add real nginx config for content

--- modules/website.nix
+++ modules/website.nix
@@ -11,9 +11,16 @@
     "www.josch557.de" = {
       forceSSL = true;
       enableACME = true;
-      locations."/" = {
-        return = "404";
-      };
+      root = "/var/www/website";
+      extraConfig = ''
+        add_header Strict-Transport-Security "max-age=31536000; includeSubDomains" always;
+        add_header X-Frame-Options "DENY" always;
+        add_header Content-Security-Policy "frame-ancestors 'none'" always;
+        add_header X-XSS-Protection "1; mode=block" always;
+        add_header X-Content-Type-Options "nosniff" always;
+
+        add_header X-Robots-Tag "noindex, nofollow" always;
+      '';
     };
   };
 }
